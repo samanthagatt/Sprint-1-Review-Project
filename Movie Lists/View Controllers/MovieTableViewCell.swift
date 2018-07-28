79a470c7a0668bb792fc3bd33900b36693d15a75
Samantha Gatt <smfgatt@gmail.com>
2018-07-28 10:19:42 -0400

    Creates storyboards

---
+++ Movie Lists/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  Movie Lists
+//
+//  Created by Samantha Gatt on 7/28/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
