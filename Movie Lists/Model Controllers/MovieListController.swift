3a9b69b2a25174b9809f6f850e8062fc7eee6d9d
Samantha Gatt <smfgatt@gmail.com>
2018-07-29 09:32:51 -0400

    Creates movie and movie list models

---
+++ Movie Lists/Model Controllers/MovieListController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListController.swift
+//  Movie Lists
+//
+//  Created by Samantha Gatt on 7/29/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
---
+++ Movie Lists/Models/MovieList.swift
@@ -0,0 +1,14 @@
+//
+//  MovieList.swift
+//  Movie Lists
+//
+//  Created by Samantha Gatt on 7/29/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+struct MovieList {
+    var name: String
+    var moviesInList: [Movie]
+}
